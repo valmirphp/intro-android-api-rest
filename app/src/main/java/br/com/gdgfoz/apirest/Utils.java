5f157230c4f2fda04d457b1792a54617a67f4259
valmirphp <valmir.php@gmail.com>
2015-12-07 21:06:15 -0200

    Update Utils.java

--- app/src/main/java/br/com/gdgfoz/apirest/Utils.java
+++ app/src/main/java/br/com/gdgfoz/apirest/Utils.java
@@ -4,6 +4,6 @@ package br.com.gdgfoz.apirest;
  * Created by hussan on 12/3/15.
  */
 public class Utils {
-    public static final String PERSON_API = "http://www.desaparecidosbr.org/api";
+    public static final String PERSON_API = "http://www.xxxx.xxx/api";
     public static final String PERSON_KEY = "person";
 }
